726069a8210b3e3a6e6bf298431e6f3a0f28e88a
Henrique Ferreiro <hferreiro@igalia.com>
2018-01-12 11:25:41 +0100

    CredentialsContainer calls mojom::CredentialManager directly
    https://crrev.com/593cc44ba056

--- brave/browser/password_manager/brave_password_manager_client.h
+++ brave/browser/password_manager/brave_password_manager_client.h
@@ -154,7 +154,7 @@ class BravePasswordManagerClient
   void SetTestObserver(autofill::PasswordGenerationPopupObserver* observer);
 
   static void BindCredentialManager(
-      password_manager::mojom::CredentialManagerAssociatedRequest request,
+      password_manager::mojom::CredentialManageredRequest request,
       content::RenderFrameHost* render_frame_host);
 
   // A helper method to determine whether a save/update bubble can be shown
